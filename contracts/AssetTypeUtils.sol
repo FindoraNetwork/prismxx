e26ffbdbcad8f783663538a6a08db4bcaf44081c
shaorongqiang <shaorongqiang@aliyun.com>
2023-04-11 11:39:36 +0800

    fix hash param

--- contracts/AssetTypeUtils.sol
+++ contracts/AssetTypeUtils.sol
@@ -26,8 +26,22 @@ contract AssetTypeUtils {
         address addr,
         uint256 tokenId
     ) public view returns (bytes32 result) {
+        bytes32 code = bytes32(tokenId);
+        bytes32 code0 = 0x00;
+        bytes32 code1 = 0x00;
+        bytes memory tmp0 = new bytes(32);
+        bytes memory tmp1 = new bytes(32);
+        uint i = 0;
+        for (i = 0; i < 31; i++) {
+            tmp0[i] = code[i];
+        }
+        tmp1[0] = code[31];
+        assembly {
+            code0 := mload(add(tmp0, 32))
+            code1 := mload(add(tmp1, 32))
+        }
         (bool success, bytes memory source) = _anemoi_address.staticcall(
-            abi.encode(ERC721_PREFIX, addr, tokenId)
+            abi.encode(ERC721_PREFIX, addr, code0, code1)
         );
         require(success && source.length == 32, "Precompile call failed");
         assembly {
@@ -39,8 +53,22 @@ contract AssetTypeUtils {
         address addr,
         uint256 tokenId
     ) public view returns (bytes32 result) {
+        bytes32 code = bytes32(tokenId);
+        bytes32 code0 = 0x00;
+        bytes32 code1 = 0x00;
+        bytes memory tmp0 = new bytes(32);
+        bytes memory tmp1 = new bytes(32);
+        uint i = 0;
+        for (i = 0; i < 31; i++) {
+            tmp0[i] = code[i];
+        }
+        tmp1[0] = code[31];
+        assembly {
+            code0 := mload(add(tmp0, 32))
+            code1 := mload(add(tmp1, 32))
+        }
         (bool success, bytes memory source) = _anemoi_address.staticcall(
-            abi.encode(ERC1155_PREFIX, addr, tokenId)
+            abi.encode(ERC1155_PREFIX, addr, code0, code1)
         );
         require(success && source.length == 32, "Precompile call failed");
         assembly {
